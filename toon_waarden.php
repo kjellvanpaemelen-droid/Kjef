c713dd1980b30c36400eeac24d3688829fd8f694
kjellvanpaemelen-droid <kjellvanpaemelen@gmail.com>
2026-03-09 09:33:54 +0100

    Add files via upload

---
+++ toon_waarden.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Formulieren</title>
+	</head>
+
+	<body>
+
+
+		<?php
+		
+        print "<pre>" ;
+        print_r ($_POST) ;
+        print "</pre>" ;
+
+        ?>
+		
+	   	
+	</body>	
+	
+</html>
